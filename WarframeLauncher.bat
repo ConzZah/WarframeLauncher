f6ea82f8f88d111f3d899af246e237dadcbf4519
ConzZah <69615452+ConzZah@users.noreply.github.com>
2024-01-11 23:25:56 +0100

    Add files via upload

---
+++ WarframeLauncher.bat
@@ -0,0 +1,7 @@
+@echo off
+echo Warframe-Pw-goes-here| clip
+echo Password copied to clipboard!
+cd %USERPROFILE%\AppData\Local\Warframe\Downloaded\Public\Tools\
+echo Launching Launcher..
+Launcher.exe
+exit
