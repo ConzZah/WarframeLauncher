901ccaef55efde438655971f03d463fe43adefd2
ConzZah <69615452+ConzZah@users.noreply.github.com>
2024-01-12 11:30:26 +0100

    Updated WarframeLauncher.bat to v0.2
    v0.2:
    https://github.com/ConzZah/WarframeLauncher/releases/tag/v0.2
    v0.1:
    https://github.com/ConzZah/WarframeLauncher/releases/tag/v0.1

--- WarframeLauncher.bat
+++ WarframeLauncher.bat
@@ -1,7 +1,29 @@
 @echo off
-echo Warframe-Pw-goes-here| clip
-echo Password copied to clipboard!
+color 0a
+:main_menu
+echo  _    _    __    ____  ____  ____    __    __  __  ____      __      __    __  __  _  _   ___  _   _  ____  ____ 
+echo ( \/\/ )  /__\  (  _ \( ___)(  _ \  /__\  (  \/  )( ___)    (  )    /__\  (  )(  )( \( ) / __)( )_( )( ___)(  _ \
+echo  )    (  /(  )\  )   / )__)  )   / /(  )\  )    (  )__)      )(__  /(  )\  )(__)(  )  ( ( (__  ) _ (  )__)  )   /
+echo (__/\__)(__)(__)(_)\_)(_)   (_)\_)(__)(__)(_/\/\_)(____)    (____)(__)(__)(______)(_)\_) \___)(_) (_)(____)(_)\_)  v0.2 
+echo                                                                                                                  ConzZah
+echo.
+echo.
+echo                                     */// PRESS [ ENTER ] TO LAUNCH WARFRAME \\\*
+echo                                    */// ------------------------------------ \\\*
+echo                                   *///      PRESS [ X + ENTER ] TO QUIT       \\\*
+set /p userinput=
+if "%userinput%"=="y" goto main_task
+if "%userinput%"=="x" goto exit
+:main_task 
+echo Warframe-Password-goes-here| clip
 cd %USERPROFILE%\AppData\Local\Warframe\Downloaded\Public\Tools\
-echo Launching Launcher..
+cls
+echo  _    _    __    ____  ____  ____    __    __  __  ____      __      __    __  __  _  _   ___  _   _  ____  _  _   ___ 
+echo ( \/\/ )  /__\  (  _ \( ___)(  _ \  /__\  (  \/  )( ___)    (  )    /__\  (  )(  )( \( ) / __)( )_( )(_  _)( \( ) / __)
+echo  )    (  /(  )\  )   / )__)  )   / /(  )\  )    (  )__)      )(__  /(  )\  )(__)(  )  ( ( (__  ) _ (  _)(_  )  ( ( (_-..
+echo (__/\__)(__)(__)(_)\_)(_)   (_)\_)(__)(__)(_/\/\_)(____)    (____)(__)(__)(______)(_)\_) \___)(_) (_)(____)(_)\_) \___/
+echo.
+echo                                     */// Password copied to clipboard! \\\*
 Launcher.exe
+:exit
 exit
