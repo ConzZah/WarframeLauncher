a8969a48c3bc85777e2ca62eb2154ea079cb600c
ConzZah <69615452+ConzZah@users.noreply.github.com>
2024-01-26 18:52:28 +0100

    Updated WarframeLauncher.bat to v0.3

--- WarframeLauncher.bat
+++ WarframeLauncher.bat
@@ -1,29 +1,16 @@
 @echo off
 color 0a
-:main_menu
+echo Warframe-Password-goes-here| clip
 echo  _    _    __    ____  ____  ____    __    __  __  ____      __      __    __  __  _  _   ___  _   _  ____  ____ 
 echo ( \/\/ )  /__\  (  _ \( ___)(  _ \  /__\  (  \/  )( ___)    (  )    /__\  (  )(  )( \( ) / __)( )_( )( ___)(  _ \
 echo  )    (  /(  )\  )   / )__)  )   / /(  )\  )    (  )__)      )(__  /(  )\  )(__)(  )  ( ( (__  ) _ (  )__)  )   /
-echo (__/\__)(__)(__)(_)\_)(_)   (_)\_)(__)(__)(_/\/\_)(____)    (____)(__)(__)(______)(_)\_) \___)(_) (_)(____)(_)\_)  v0.2 
+echo (__/\__)(__)(__)(_)\_)(_)   (_)\_)(__)(__)(_/\/\_)(____)    (____)(__)(__)(______)(_)\_) \___)(_) (_)(____)(_)\_) v0.3 
 echo                                                                                                                  ConzZah
 echo.
 echo.
-echo                                     */// PRESS [ ENTER ] TO LAUNCH WARFRAME \\\*
-echo                                    */// ------------------------------------ \\\*
-echo                                   *///      PRESS [ X + ENTER ] TO QUIT       \\\*
-set /p userinput=
-if "%userinput%"=="y" goto main_task
-if "%userinput%"=="x" goto exit
-:main_task 
-echo Warframe-Password-goes-here| clip
+echo   *///      LAUNCHING WARFRAME      \\\*
+echo  */// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ \\\*
+echo *///   Password copied to clipboard!  \\\*
 cd %USERPROFILE%\AppData\Local\Warframe\Downloaded\Public\Tools\
-cls
-echo  _    _    __    ____  ____  ____    __    __  __  ____      __      __    __  __  _  _   ___  _   _  ____  _  _   ___ 
-echo ( \/\/ )  /__\  (  _ \( ___)(  _ \  /__\  (  \/  )( ___)    (  )    /__\  (  )(  )( \( ) / __)( )_( )(_  _)( \( ) / __)
-echo  )    (  /(  )\  )   / )__)  )   / /(  )\  )    (  )__)      )(__  /(  )\  )(__)(  )  ( ( (__  ) _ (  _)(_  )  ( ( (_-..
-echo (__/\__)(__)(__)(_)\_)(_)   (_)\_)(__)(__)(_/\/\_)(____)    (____)(__)(__)(______)(_)\_) \___)(_) (_)(____)(_)\_) \___/
-echo.
-echo                                     */// Password copied to clipboard! \\\*
 Launcher.exe
-:exit
 exit
